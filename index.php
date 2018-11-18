164312f29c0880a9bed30920250b9f92572ac57f
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-18 13:56:45 -0500

    First Upload
    uploading original files

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Linux</title>
+    
+    <link rel="stylesheet" type="text/css" href="proj.css" />
+</head>
+<body>
+	<header>
+       <?php 
+        require "navbar.html";
+        ?>
+        
+        
+    </header>
+    <div class="parallax" id="parallax1">
+        <div class="section" id="sec1">
+            <div class="left">
+                <div class="leftcontent" id="leftcontent1">
+                    <h1>LEARN LINUX</h1>
+                    <p>bghj uikmn bhuikmnb huknbh hjnbh jhhj s daksnf wu fpalnc eiru vlsdn gurhoaks vureid fughjldjsldf ewoifsd e d fewijds foewoi</p>
+                    <br />
+                    <a href="#">DOWNLOAD</a>
+                </div>
+            </div>
+            <div class="right">
+                <!--space holder-->
+            </div>
+        </div>
+    </div>
+    <div class="section" id="sec2">
+        <div class="left" id="left2">
+            <div class="leftcontent" id="leftcontent2">
+                <h1>EXPLORE</h1>
+                <p>OUR SITE</p>
+            </div>
+        </div>
+        <div class="right">
+            <ul id="left-list">
+                <div>
+                    <li>ABOUT</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+                <div>
+                    <li>BUILD SCRIPTS</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+                <div>
+                    <li>DOWNLOAD</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+            </ul>            
+            <ul id="right-list">
+                <div>
+                    <li>COMMAND LINE</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+                <div>
+                    <li>DISTRIBUTIONS</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+            </ul>
+        </div>
+    </div>
+    <div class="parallax" id="parallax2">
+        <!--space holder-->
+    </div>
+    <div class="section" id="sec3">
+        <h2>OUR PROJECT</h2>
+        <p>shbdsa sdkjfhjoe sksdjfnio rnrjnsdio nfkdf ejvnj fksbvsdjc ljhfg sdbvibd nmnvfvjsd vkjbeh sdnnf dj fnfjej jd sovbej  eknv wekjvbj dsa sdkjfhjoe sksdjfnio rnrjnsdio nfkdf ejvnj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvj fksbvsdjc ljhfg sdbvibd nmnvfvjsd vkjbeh sdnnf dj fnfjej jd sovb knv</p>
+    </div>
+    <div class="parallax" id="parallax3">
+        <!--space holder-->
+    </div>
+    <div class="section" id="sec4">
+        <a href="#">BACK TO TOP &#8593;</a>
+    </div>
+    <footer>
+        <p>@2003 Team 3 Final Project | Web and Mobile II</p>
+    </footer>
+</body>
+</html>
