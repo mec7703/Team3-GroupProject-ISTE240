bf7d434e5c59a468d48d139ef8bc013db8c2006d
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-29 09:41:50 -0500

    Update and rename commandsPage.php to begin.php

---
+++ begin.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Linux</title>
+    
+    <link rel="stylesheet" type="text/css" href="./assets/css/proj.css" />
+</head>
+<body>
+	<header>
+       <?php 
+        require "navbar.html";
+        ?>
+    
+  </body>
+</html>
