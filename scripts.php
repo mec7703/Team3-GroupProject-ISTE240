5961d640f924bb422cb4fd0ab3e4035c71eb39bc
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-29 09:44:47 -0500

    Update and rename scriptsPage.php to scripts.php

---
+++ scripts.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Build Scripts</title>
+    
+    <link rel="stylesheet" type="text/css" href="./assets/css/proj.css" />
+</head>
+<body>
+	<header>
+       <?php 
+        require "navbar.html";
+        ?>
+  
+  </body>
+</html>
