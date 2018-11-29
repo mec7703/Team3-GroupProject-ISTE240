92206a8115e5f3db17ebecdc62e4ba7fda02114f
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-29 10:18:12 -0500

    created contact form

--- contact.php
+++ contact.php
@@ -12,39 +12,41 @@
         require "navbar.html";
         ?>
   </header>
-	<div class="section" id="sec2">
-        <div class="left" id="left2">
-            <div class="leftcontent" id="leftcontent2">
-                <h2 class="mini">EXPLORE</h2>
-                <p>OUR SITE</p>
+  <div class="parallax" id="parallax1">
+        <div class="section" id="sec1">
+            <div class="left">
+                <div class="leftcontent" id="leftcontent1">
+                    <h2 class="title">CONTACT US</h2>
+                    <p>We encourage you to reach out and contact us with any questions or suggestions.</p>
+                </div>
+            </div>
+            <div class="right">
+                <!--space holder-->
             </div>
         </div>
-        <div class="right">
-            <ul id="left-list">
-                <div>
-                    <li>ABOUT</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-                <div>
-                    <li>BUILD SCRIPTS</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-                <div>
-                    <li>DOWNLOAD</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-            </ul>            
-            <ul id="right-list">
-                <div>
-                    <li>COMMAND LINE</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-                <div>
-                    <li>DISTRIBUTIONS</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-            </ul>
+    </div>
+    <div class="section" id="sec2">
+        <div class="left" id="left2">
+            <div class="leftcontent" id="leftcontent2">
+                <h2 class="mini">MESSAGE</h2>
+                <p>OUR TEAM</p>
+            </div>
         </div>
+            <div class="right">
+                    <div class="review">
+                        <h3>SUBMIT YOUR COMMENTS HERE</h3>
+                        <!--get input from user and add to the database-->
+                        <form action="comments.php" method="GET">		
+                            Name: <input type ="text" id="name" name="name" width="40" placeholder="Enter your name" />
+                            <p>&nbsp;</p>
+                            Comment: <br /> <textarea id="comemnt" name="comment" cols="99" rows="10"></textarea>
+                            <p><input class = "sumbit" type = "submit" value="submit comment"/></p>	
+                        </form>
+                    </div>
+        </div>  
+    </div>
+    <div class="parallax" id="parallax2">
+        <!--space holder-->
     </div>
 </body>
 </html>
