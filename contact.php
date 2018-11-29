868ca0bc3fc570e8ce3adffe82fc5ba551560783
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-29 09:51:37 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -12,5 +12,39 @@
         require "navbar.html";
         ?>
   </header>
+	<div class="section" id="sec2">
+        <div class="left" id="left2">
+            <div class="leftcontent" id="leftcontent2">
+                <h2 class="mini">EXPLORE</h2>
+                <p>OUR SITE</p>
+            </div>
+        </div>
+        <div class="right">
+            <ul id="left-list">
+                <div>
+                    <li>ABOUT</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+                <div>
+                    <li>BUILD SCRIPTS</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+                <div>
+                    <li>DOWNLOAD</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+            </ul>            
+            <ul id="right-list">
+                <div>
+                    <li>COMMAND LINE</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+                <div>
+                    <li>DISTRIBUTIONS</li>
+                    <p>small description about what the about tab will include</p>
+                </div>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
