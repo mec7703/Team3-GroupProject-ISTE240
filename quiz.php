cfa07ddb596c0137e00852272bf4e8c4a568c66f
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-30 13:24:10 -0500

    update quiz.html

--- quiz.php
+++ quiz.php
@@ -1,6 +1,41 @@
-<div class="leftcontent" id="leftcontent1">
-                    <h2 class = "title">LEARNLNX</h2>
-                    <p>Learn and test your Linux knowledge with learnlnx</p>
-                    <br />
-                    <a class = "download" href="#">DOWNLOAD</a>
-                </div>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>Linux</title>
+    
+    <link rel="stylesheet" type="text/css" href="./assets/css/proj.css" />
+</head>
+<body>
+	<header>
+       <?php 
+        require "navbar.html";
+        ?>
+  </header>
+<div class="container">
+    <div class="quizbox">
+        <div class = "progress">
+                <div class="grid-item1">33%</div>
+                <div class="grid-item2"></div>
+                <div class="grid-item3"></div>     
+        </div>
+        <h4>Question: </h4>
+        <br>
+        <p><!--php echo for question--></p>
+        
+         <div class="review">
+    <h4>Your Answer</h4>
+    <!--get input from user and add to the database-->
+<form action="quiz.php" method="GET">		
+   <br /> <textarea id="comemnt" name="comment" cols="99" rows="10">
+    </textarea>
+    <p><input type = "submit" value="Check Answer"/></p>	
+</form>
+        </div>
+    </div>
+  
+</div>
+</body>
+    
+</html>
