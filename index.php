ffbc2cca48c16c8b2b514e9dcd95e8ac2504c8b9
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-30 12:09:00 -0500

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     </div>
     <div class="section" id="sec3">
         <h2>OUR PROJECT</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Our project is a Linux educational site that is deisgned for a wide range of user demographics to help them in developing a base understanding of Linux and its applications. </p>
         <br>
            <div class="row2">
   <div class="column2">
