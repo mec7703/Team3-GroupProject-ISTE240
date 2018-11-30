8e4e829a6f13b1065c15458ae5e17d4069e156e0
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-30 12:04:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,61 +14,56 @@
         
         
     </header>
-    <div class="parallax" id="parallax1">
-        <div class="section" id="sec1">
-            <div class="left">
-                <div class="leftcontent" id="leftcontent1">
-                    <h2 class="title">LEARN LINUX</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <br />
-                    <a href="#">DOWNLOAD</a>
-                </div>
-            </div>
-            <div class="right">
-                <!--space holder-->
-            </div>
-        </div>
-    </div>
-    <div class="section" id="sec2">
-        <div class="left" id="left2">
-            <div class="leftcontent" id="leftcontent2">
-                <h2 class="mini">EXPLORE</h2>
-                <p>OUR SITE</p>
-            </div>
-        </div>
-        <div class="right">
-            <ul id="left-list">
-                <div>
-                    <li>ABOUT</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-                <div>
-                    <li>BUILD SCRIPTS</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-                <div>
-                    <li>DOWNLOAD</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-            </ul>            
-            <ul id="right-list">
-                <div>
-                    <li>COMMAND LINE</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-                <div>
-                    <li>DISTRIBUTIONS</li>
-                    <p>small description about what the about tab will include</p>
-                </div>
-            </ul>
-        </div>
-    </div>
-    <div class="parallax" id="parallax2">
+  <div class="parallax" id="parallax2">
         <!--space holder-->
     </div>
     <div class="section" id="sec3">
         <h2>OUR PROJECT</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <br>
+           <div class="row2">
+  <div class="column2">
+    <div class="content2">
+      <img src="./assets/images/icon.png" alt="Mountains" >
+      <h3>Matthew Rutter</h3>
+        <h4>Team Leader/ Content Creator</h4>
+      <p class="dec">I am one of the project team leaders and a content creator for the site. My responsibilities include delegating tasks, research and php implementation.</p>
+    </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+    <img src="./assets/images/icon.png" alt="Lights" >
+      <h3>Emily Mills</h3>
+        <h4>Team Leader/ Graphic Designer</h4>
+      <p class="dec">I am one of the project team leaders and the primary UI designer for the site. My responsibilities include delegating tasks, logo creation, site navigation and overseeing user flow.</p>
+    </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+    <img src="./assets/images/icon.png" alt="Nature" >
+      <h3>Mike Reniere</h3>
+             <h4>Graphic Designer/ Content Creator</h4>
+      <p class="dec">I am one of the graphic designers and a content creator for the site. My responsibilities include creating graphic elements and videos, research and php implementation.</p>
+    </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+    <img src="./assets/images/icon.png" alt="Mountains" >
+      <h3>Jamie Rodriguez</h3>
+             <h4>Information Architect/ Interaction Design</h4>
+      <p class="dec">I am the sole information architect and an interaction designer for the site. My responsibilities include establishing user flow and navigation and creating interactive visual content. </p>
+    </div>
+  </div>
+               <div class="column2">
+    <div class="content2">
+    <img src="./assets/images/icon.png" alt="Mountains" >
+      <h3>Mitchell Cartner</h3>
+             <h4>Interaction Designer/ Graphic Design</h4>
+      <p class="dec">I am an interaction designer and a graphic designer for the site. My responsibilities include creating interactive visual content and creating demonstration videos.</p>
+    </div>
+  </div>
+<!-- END GRID -->
+</div>
     </div>
     <div class="parallax" id="parallax3">
         <!--space holder-->
@@ -77,7 +72,7 @@
         <a href="#">Back to Top &#8593;</a>
     </div>
     <footer>
-        <p class="footer">@2018 Team 3 Final Project | Web and Mobile II</p>
+        <p>@2003 Team 3 Final Project | Web and Mobile II</p>
     </footer>
 </body>
 </html>
