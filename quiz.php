0fba400f678fbfd768c351f39b9ecf8e88340d17
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-11-30 12:18:32 -0500

    create quiz.php

---
+++ quiz.php
@@ -0,0 +1,6 @@
+<div class="leftcontent" id="leftcontent1">
+                    <h2 class = "title">LEARNLNX</h2>
+                    <p>Learn and test your Linux knowledge with learnlnx</p>
+                    <br />
+                    <a class = "download" href="#">DOWNLOAD</a>
+                </div>
