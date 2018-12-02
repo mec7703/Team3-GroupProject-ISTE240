7cdf1d5980d7fefa1bf3a1c51fa213f77208350c
exm7057 <45149750+exm7057@users.noreply.github.com>
2018-12-02 12:45:24 -0500

    coded distributions page

--- distributions.php
+++ distributions.php
@@ -12,6 +12,232 @@
         require "navbar.html";
         ?>
 	</header>
+
+    <body>
+        <div class="dist">
+        <h2>Download Linux</h2>
+        <p class="para">Click the links below to download Linux for your machine</p>
+   <div class="row2">
+  <div class="column2">
+    <div class="content2">
+        <div class="container">
+      <img src="./assets/images/ubuntu.png" alt="Mountains" style="width:100%">
+           <a href="https://www.ubuntu.com/download">Download Here</a>
+      </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/centos.png" alt="Lights" style="width:100%">
+      <a href="https://wiki.centos.org/Download">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/debian.png" alt="Nature" style="width:100%">
+      <a href="https://www.debian.org/distrib/ftplist">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/fedora.png" alt="Mountains" style="width:100%">
+      <a href="https://getfedora.org">Download Here</a>
+    </div>
+      </div>
+  </div>
+   <div class="column2">
+    <div class="content2">
+         <div class="container">
+      <img src="./assets/images/slackware.png" alt="Mountains" style="width:100%">
+    <a href="http://www.slackware.com/getslack/">Download Here</a>
+    </div>
+       </div>
+  </div>
+<!-- END GRID -->
+</div>
+   <div class="row2">
+  <div class="column2">
+    <div class="content2">
+        <div class="container">
+      <img src="./assets/images/mint.png" alt="Mountains" style="width:100%">
+           <a href="https://www.linuxmint.com/download.php">Download Here</a>
+      </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/xubuntu.png" alt="Lights" style="width:100%">
+      <a href="https://xubuntu.org/download">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/arch.png" alt="Nature" style="width:100%">
+      <a href="https://www.archlinux.org/download/">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/opensuse.png" alt="Mountains" style="width:100%">
+      <a href="https://software.opensuse.org">Download Here</a>
+    </div>
+      </div>
+  </div>
+   <div class="column2">
+    <div class="content2">
+         <div class="container">
+      <img src="./assets/images/redhat.png" alt="Mountains" style="width:100%">
+    <a href="https://access.redhat.com/downloads">Download Here</a>
+    </div>
+       </div>
+  </div>
+<!-- END GRID -->
+</div>
+          <div class="row2">
+  <div class="column2">
+    <div class="content2">
+        <div class="container">
+      <img src="./assets/images/slackel.png" alt="Mountains" style="width:100%">
+           <a href="https://sourceforge.net/projects/slackel/files/">Download Here</a>
+      </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/pure.png" alt="Lights" style="width:100%">
+      <a href="https://pureos.net/download/">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/mageia.png" alt="Nature" style="width:100%">
+      <a href="https://www.mageia.org/en/downloads/">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/pclinux.png" alt="Mountains" style="width:100%">
+      <a href="https://www.pclinuxos.com/get-pclinuxos/">Download Here</a>
+    </div>
+      </div>
+  </div>
+   <div class="column2">
+    <div class="content2">
+         <div class="container">
+      <img src="./assets/images/puppy.png" alt="Mountains" style="width:100%">
+    <a href="http://puppylinux.com/download">Download Here</a>
+    </div>
+       </div>
+  </div>
+<!-- END GRID -->
+</div>
+          <div class="row2">
+  <div class="column2">
+    <div class="content2">
+        <div class="container">
+      <img src="./assets/images/zorin.png" alt="Mountains" style="width:100%">
+           <a href="https://zorinos.com/download/">Download Here</a>
+      </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/bunsen.png" alt="Lights" style="width:100%">
+      <a href="https://www.bunsenlabs.org/installation.html">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/kubuntu.png" alt="Nature" style="width:100%">
+      <a href="https://kubuntu.org/getkubuntu/">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/manjaro.png" alt="Mountains" style="width:100%">
+      <a href="https://sourceforge.net/projects/manjarolinux/files/">Download Here</a>
+    </div>
+      </div>
+  </div>
+   <div class="column2">
+    <div class="content2">
+         <div class="container">
+      <img src="./assets/images/bodhi.png" alt="Mountains" style="width:100%">
+    <a href="https://sourceforge.net/projects/bodhilinux/files/">Download Here</a>
+    </div>
+       </div>
+  </div>
+<!-- END GRID -->
+</div>
+           <div class="row2">
+  <div class="column2">
+    <div class="content2">
+        <div class="container">
+      <img src="./assets/images/netrunner.png" alt="Mountains" style="width:100%">
+           <a href="https://www.netrunner.com/download/">Download Here</a>
+      </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/neptune.png" alt="Lights" style="width:100%">
+      <a href="https://neptuneos.com/en/download.html">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/kali.png" alt="Nature" style="width:100%">
+      <a href="https://www.kali.org/downloads/">Download Here</a>
+    </div>
+      </div>
+  </div>
+  <div class="column2">
+    <div class="content2">
+         <div class="container">
+    <img src="./assets/images/blacklab.png" alt="Mountains" style="width:100%">
+      <a href="https://sourceforge.net/projects/os4online/files/">Download Here</a>
+    </div>
+      </div>
+  </div>
+   <div class="column2">
+    <div class="content2">
+         <div class="container">
+      <img src="./assets/images/solus.png" alt="Mountains" style="width:100%">
+    <a href="https://getsol.us/download/">Download Here</a>
+    </div>
+       </div>
+  </div>
+<!-- END GRID -->
+</div>
+            </div>
+
+        <footer>
+            <p><em>Individual Midterm Project</em><br/>
+  		    <em>Created by Emily Mills &copy;2018</em></p>
+        </footer>
   
   </body>
 </html>
