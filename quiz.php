39833d6fd4735cd91205f3043cad1edce5f051b4
mec7703 <mec7703@g.rit.edu>
2018-12-02 12:36:44 -0500

    Fixed header include link

--- quiz.php
+++ quiz.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" type="text/css" href="./assets/css/proj.css" />
 </head>
 <body>
-	<header><?php require "navbar.html"; ?></header>
+	<header><?php require "assets/inc/navbar.html"; ?></header>
     
 <?php
 
