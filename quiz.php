50b23316968054343984652626c36538369f08dd
jrr3147 <45493112+jrr3147@users.noreply.github.com>
2018-12-02 13:28:36 -0500

    Add files via upload

--- quiz.php
+++ quiz.php
@@ -5,69 +5,72 @@
 	<meta charset="utf-8" />
 	<title>Linux</title>
     
-    <script src="quiz.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-    <link rel="stylesheet" type="text/css" href="./assets/css/proj.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/proj.css" />
+    
+    <script src="assets/script/quiz.js"></script>
 </head>
 <body>
 	<header><?php require "assets/inc/navbar.html"; ?></header>
     
-<?php
+    <?php
+    $path = "./";
+    require $path."../../../dbConnect.inc";
 
-$path = "./";
-require $path."../../../dbConnect.inc";
-    
-session_start();
+    session_start();
 
-//get quiz stuff from the database
-$stmt = "SELECT question,answer FROM quiz";
-$result = $mysqli->query($stmt);
+    //get quiz stuff from the database
+    $stmt = "SELECT question,answer FROM quiz";
+    $result = $mysqli->query($stmt);
 
-if(!isset($quiz) && !isset($questions)) {
-//empty array to store stuff
-    $questions = array();
+    if(!isset($quiz) && !isset($questions)) {
+    //empty array to store stuff
+        $questions = array();
 
-//put the questions and answers in the associative array
-    if ($result->num_rows > 0) {
-        while ($row = $result->FETCH_ASSOC()) {
-            $new_input = array($row['question'] => $row['answer']);
-            $questions = array_merge($questions, $new_input);
+    //put the questions and answers in the associative array
+        if ($result->num_rows > 0) {
+            while ($row = $result->FETCH_ASSOC()) {
+                $new_input = array($row['question'] => $row['answer']);
+                $questions = array_merge($questions, $new_input);
+            }
+        } else {
+            echo "No results found";
         }
-    } else {
-        echo "No results found";
-    }
 
-//empty final array
-    $quiz = array();
+    //empty final array
+        $quiz = array();
 
-    $i = 0;//index var for loop
-    while ($i < 3) {//loop through and grab random questions and answers; num in num of questions to display
-        $key = array_rand($questions);//grab key
-        $value = $questions[$key];//grab value
-        $new_input = array($key => $value);//make new assoc array
-        $quiz = array_merge($quiz, $new_input);//merge arrays to add to final quiz array
-        $i++;
-    }
-    
-//store quiz questions/answers in session var
-$_SESSION['quiz']=$quiz;
-}?>
+        $i = 0;//index var for loop
+        while ($i < 3) {//loop through and grab random questions and answers; num in num of questions to display
+            $key = array_rand($questions);//grab key
+            $value = $questions[$key];//grab value
+            $new_input = array($key => $value);//make new assoc array
+            $quiz = array_merge($quiz, $new_input);//merge arrays to add to final quiz array
+            $i++;
+        }
+
+    //store quiz questions/answers in session var
+    $_SESSION['quiz']=$quiz;
+    }?>
     
 <div class="container">
     <div class="quizbox">
         <div class = "progress">
-                <div class="grid-item1">33%</div>
-                <div class="grid-item2"></div>
-                <div class="grid-item3"></div>     
+                <div id="grid-item1">33%</div>
+                <div id="grid-item2"></div>
+                <div id="grid-item3"></div>     
         </div>
         <h4>Question: </h4>
         <br>
-        <p><?php //pop a question/answer set from the array and echo question
+        <p>
+            <?php //pop a question/answer set from the array and echo question
             $value = reset($quiz);
             $key = key($quiz);
             unset($quiz[$key]);
-            echo $key; ?></p>
+            echo $key; 
+            ?>
+        </p>
         
         <div class="review">
             <h4>Your Answer</h4><br>
@@ -75,14 +78,17 @@ $_SESSION['quiz']=$quiz;
             <textarea id="answer" name="<?php echo $value; ?>" cols="99" rows="10">
             </textarea><br>
             <button id="checkAns" onclick="checkAns()">Check Answer</button>
+            <button id="next" onclick="next()" style="display: none">Next Question</button>
+            <button id="retry" onclick="window.location.reload()" style="display: none">Another Quiz</button>
         </div>
-        <div id="correct" style="display:none">Correct</div>
+        <div id="correct" style="display:none"><p class=reviewAns>Correct</p></div>
         <div id="incorrect" style="display:none">
-            Incorrect: Answer is <?php echo $value; ?>
+            <p class="reviewAns">Incorrect: Answer is <?php echo $value; ?></p>
         </div>
     </div>
   
 </div>
+
 </body>
     
 </html>
