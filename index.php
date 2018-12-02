b5f53683aafbacac7030289c999023f30c345742
jrr3147 <45493112+jrr3147@users.noreply.github.com>
2018-12-02 15:04:12 -0500

    added links to explore section

--- index.php
+++ index.php
@@ -38,25 +38,25 @@
         <div class="right">
             <ul id="left-list">
                 <div>
-                    <li>ABOUT</li>
+                    <li><a href="about.php">ABOUT</a></li>
                     <p>Learn about what Linux is, its purpose and its different components</p>
                 </div>
                 <div>
-                    <li>BUILD SCRIPTS</li>
+                    <li><a href="scripts.php">BUILD SCRIPTS</a></li>
                     <p>Learn what it is and how to create your own</p>
                 </div>
                 <div>
-                    <li>DOWNLOAD</li>
+                    <li><a href="https://www.linux.org/pages/download/">DOWNLOAD</a></li>
                     <p>Instructions for how to download Linux on your local machine</p>
                 </div>
             </ul>            
             <ul id="right-list">
                 <div>
-                    <li>COMMAND TUTORIALS</li>
+                    <li><a href="begin.php">COMMAND TUTORIALS</a></li>
                     <p>Learn and then test your knwoledge on Linux commands</p>
                 </div>
                 <div>
-                    <li>DISTRIBUTIONS</li>
+                    <li><a href="distributions.php">DISTRIBUTIONS</a></li>
                     <p>Know your options and pick the one that's right for you </p>
                 </div>
             </ul>
@@ -68,12 +68,12 @@
     </div>
     <div class="section" id="sec3">
         <h2>OUR PROJECT</h2>
-        <p>Our project is a Linux educational site that is deisgned for a wide range of user demographics to help them in developing a base understanding of Linux and its applications. </p>
+        <p id="project">Our project is a Linux educational site that is deisgned for a wide range of user demographics to help them in developing a base understanding of Linux and its applications. </p>
         <br>
            <div class="row2">
   <div class="column2">
     <div class="content2">
-      <img src="./assets/img/icon.png" alt="Mountains" >
+      <img src="./assets/images/icon.png" alt="Mountains" >
       <h3>Matthew Rutter</h3>
         <h4>Team Leader/ Content Creator</h4>
       <p class="dec">I am one of the project team leaders and a content creator for the site. My responsibilities include delegating tasks, research and php implementation.</p>
@@ -81,7 +81,7 @@
   </div>
   <div class="column2">
     <div class="content2">
-    <img src="./assets/img/icon.png" alt="Lights" >
+    <img src="./assets/images/icon.png" alt="Lights" >
       <h3>Emily Mills</h3>
         <h4>Team Leader/ Graphic Designer</h4>
       <p class="dec">I am one of the project team leaders and the primary UI designer for the site. My responsibilities include delegating tasks, logo creation, site navigation and overseeing user flow.</p>
@@ -89,7 +89,7 @@
   </div>
   <div class="column2">
     <div class="content2">
-    <img src="./assets/img/icon.png" alt="Nature" >
+    <img src="./assets/images/icon.png" alt="Nature" >
       <h3>Mike Reniere</h3>
              <h4>Graphic Designer/ Content Creator</h4>
       <p class="dec">I am one of the graphic designers and a content creator for the site. My responsibilities include creating graphic elements and videos, research and php implementation.</p>
@@ -97,7 +97,7 @@
   </div>
   <div class="column2">
     <div class="content2">
-    <img src="./assets/img/icon.png" alt="Mountains" >
+    <img src="./assets/images/icon.png" alt="Mountains" >
       <h3>Jamie Rodriguez</h3>
              <h4>Information Architect/ Interaction Design</h4>
       <p class="dec">I am the sole information architect and an interaction designer for the site. My responsibilities include establishing user flow and navigation and creating interactive visual content. </p>
@@ -105,7 +105,7 @@
   </div>
                <div class="column2">
     <div class="content2">
-    <img src="./assets/img/icon.png" alt="Mountains" >
+    <img src="./assets/images/icon.png" alt="Mountains" >
       <h3>Mitchell Cartner</h3>
              <h4>Interaction Designer/ Graphic Design</h4>
       <p class="dec">I am an interaction designer and a graphic designer for the site. My responsibilities include creating interactive visual content and creating demonstration videos.</p>
