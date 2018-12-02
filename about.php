7ecc8407a0388975393103ec087fad77efc6a711
jrr3147 <45493112+jrr3147@users.noreply.github.com>
2018-12-02 13:47:25 -0500

    added the content

--- about.php
+++ about.php
@@ -11,6 +11,29 @@
        <?php 
         require "assets/inc/navbar.html";
         ?>
-  
+    </header>
+    <p>
+        Linux is a series of open-sourced operating systems (OS) all based
+        on the Linux kernel, created by Linus Torvald. A Linux kernel 
+        is basically underlying software that has a packet of programs 
+        that allows a computer to work as we know them to. It is free, 
+        fast, and secure. Unlike Windows and Apple computers, most viruses 
+        cannot run on Linux. Because it is so cheap (usually free) and 
+        secure, it is commonly used on servers and other big systems. It is 
+        also the base of many other electronic systems, like phones, video 
+        game consoles, and smartwatches.
+        <br />
+        Linux has hundreds of different versions all based on the Linux 
+        kernel. Each version/distribution is similar at its core but may 
+        look different and come with different programs already isntalled. 
+        For example, each Linux distribution may have a graphical user 
+        interface (GUI) or it may have a command line interface.
+    </p> 
+    <p>
+        A Linux Shell is a programming language that has a command line interface (typically) where the user enters an input which is then executed. Once executed, the output is displayed in the terminal. There are many different shells that can be used, and each has its own set of recognized commands and functions. 
+    </p>
+    <p>
+        The shell prompt, also called a command prompt, is entered by the shell “prompting” the user for a command. Once the user presses enter, the shell reads the command, determines what the command is, and then executes the command if able. This is the basis for creating a shell script.
+    </p>
   </body>
 </html>
