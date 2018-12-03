0005444bd2395ea7adc16c92a14032383038ede4
MikeReniere <37932322+MikeReniere@users.noreply.github.com>
2018-12-03 14:19:59 -0500

    Add files via upload

--- assets/script/videoSlide.js
+++ assets/script/videoSlide.js
@@ -1,14 +1,15 @@
 
 var navIndex = 1;
 var manIndex = 4;
-var appIndex = 7;
-var keyIndex = 10;
-var misIndex = 13;
+var appIndex = 8;
+var keyIndex = 11;
+var misIndex = 14;
 
 function showSlides(inc, min, max, sect) {
     var i = min-1;
     var slides = document.getElementsByClassName("mySlides");
-    var next = sect+inc;
+    var next = window[sect]+inc;
+    console.log("sect: " + window[sect] + " " + "next: " + next);
 
     if(next > max){//loop back to beginning
         next=min;
@@ -16,12 +17,13 @@ function showSlides(inc, min, max, sect) {
     if(next < min){//loop back to ending
         next=max;
     }
-    sect = next;
+    window[sect] = next;
 
     //sets all videos in set to display:none
-    for (i; i < max-1; i++) {
+    for (i; i < max; i++) {
         slides[i].style.display = "none";
     }
-
+ console.log("sect: " + window[sect] + " " + "next: " + next);
     slides[next-1].style.display = "block";
+   // console.log("sect: " + sect + " " + "next: " + next);
 }
