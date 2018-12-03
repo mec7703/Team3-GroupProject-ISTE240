f700cc7025789be74386558c3b6f6d3f35c91a2d
mec7703 <mec7703@g.rit.edu>
2018-12-03 17:47:22 -0500

    Updated distributions page

--- distributions.php
+++ distributions.php
@@ -13,254 +13,127 @@
         ?>
 	</header>
 
-        <div class="dist">
-        <h2>Download Linux</h2>
-        <p class="para">Click the links below to download Linux for your machine</p>
-   <div class="row2">
-  <div class="column2">
-    <div class="content2">
-        <div class="container">
-           <a href="https://www.ubuntu.com/download">
-		   <img src="./assets/images/ubuntu.png" alt="Mountains" style="height:100%; width: auto;">
-		</a>
-      </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
-      <a href="https://wiki.centos.org/Download">
-	      <img src="./assets/images/centos.png" alt="Lights" style="height:100%; width: auto;">
-		 </a>
+<div class="dist">
+    <h2>Download Linux</h2>
+    <p class="para">Click the links below to download Linux for your machine</p>
+    <div class="tableWrap">
+    <div class="container">
+       <a href="https://www.ubuntu.com/download">
+            <img src="./assets/images/ubuntu.png" alt="Mountains" ></a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
-      <a href="https://www.debian.org/distrib/ftplist">
-	      <img src="./assets/images/debian.png" alt="Nature" style="height:100%; width: auto;">
-</a>
+    <div class="container">
+        <a href="https://wiki.centos.org/Download">
+            <img src="./assets/images/centos.png" alt="Lights" ></a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
+    <div class="container">
+        <a href="https://www.debian.org/distrib/ftplist">
+            <img src="./assets/images/debian.png" alt="Nature" ></a>
+    </div>
+    <div class="container">
       <a href="https://getfedora.org">
-	      <img src="./assets/images/fedora.png" alt="Mountains" style="height:100%; width: auto;">
-</a>
+	      <img src="./assets/images/fedora.png" alt="Mountains" ></a>
     </div>
-      </div>
-  </div>
-   <div class="column2">
-    <div class="content2">
-         <div class="container">
+    <div class="container">
     <a href="http://www.slackware.com/getslack/">
-	    <img src="./assets/images/slackware.png" alt="Mountains" style="height:100%; width: auto;">
-</a>
+	    <img src="./assets/images/slackware.png" alt="Mountains" ></a>
     </div>
-       </div>
-  </div>
-<!-- END GRID -->
-</div>
-   <div class="row2">
-  <div class="column2">
-    <div class="content2">
-        <div class="container">
-           <a href="https://www.linuxmint.com/download.php">
-		   <img src="./assets/images/mint.png" alt="Mountains" style="height:100%; width: auto;">
-</a>
-      </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
-      <a href="https://xubuntu.org/download">
-	      <img src="./assets/images/xubuntu.png" alt="Lights" style="height:100%; width: auto;">
-</a>
+    <div class="container">
+        <a href="https://www.linuxmint.com/download.php">
+		   <img src="./assets/images/mint.png" alt="Mountains" ></a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
-      <a href="https://www.archlinux.org/download/">
-	      <img src="./assets/images/arch.png" alt="Nature" style="height:100%; width: auto;">
-</a>
+    <div class="container">
+        <a href="https://xubuntu.org/download">
+	       <img src="./assets/images/xubuntu.png" alt="Lights" ></a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
-      <a href="https://software.opensuse.org">
-	      <img src="./assets/images/opensuse.png" alt="Mountains" style="height:100%; width: auto;">
-</a>
+    <div class="container">
+        <a href="https://www.archlinux.org/download/">
+	       <img src="./assets/images/arch.png" alt="Nature" ></a>
     </div>
-      </div>
-  </div>
-   <div class="column2">
-    <div class="content2">
-         <div class="container">
-    <a href="https://access.redhat.com/downloads">
-	    <img src="./assets/images/redhat.png" alt="Mountains" style="height:100%; width: auto;">
-</a>
+    <div class="container">
+        <a href="https://software.opensuse.org">
+	      <img src="./assets/images/opensuse.png" alt="Mountains" ></a>
     </div>
-       </div>
-  </div>
-<!-- END GRID -->
-</div>
-          <div class="row2">
-  <div class="column2">
-    <div class="content2">
-        <div class="container">
-           <a href="https://sourceforge.net/projects/slackel/files/">
-		   <img src="./assets/images/slackel.png" alt="Mountains" style="height:100%; width: auto;">
-</a>
-      </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
-      <a href="https://pureos.net/download/">
-	      <img src="./assets/images/pure.png" alt="Lights" style="height:100%; width: auto;">
-</a>
+    <div class="container">
+        <a href="https://access.redhat.com/downloads">
+	       <img src="./assets/images/redhat.png" alt="Mountains" ></a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
-      <a href="https://www.mageia.org/en/downloads/">
-	      <img src="./assets/images/mageia.png" alt="Nature" style="height:100%; width: auto;">
-</a>
+    <div class="container">
+        <a href="https://sourceforge.net/projects/slackel/files/">
+		   <img src="./assets/images/slackel.png" alt="Mountains" ></a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
-         <div class="container">
-      <a href="https://www.pclinuxos.com/get-pclinuxos/">
-	      <img src="./assets/images/pclinux.png" alt="Mountains" style="height:100%; width: auto;">
-</a>
+    <div class="container">
+        <a href="https://pureos.net/download/">
+	       <img src="./assets/images/pure.png" alt="Lights" ></a>
     </div>
-      </div>
-  </div>
-   <div class="column2">
-    <div class="content2">
+    <div class="container">
+        <a href="https://www.mageia.org/en/downloads/">
+	       <img src="./assets/images/mageia.png" alt="Nature" ></a>
+    </div>
+    <div class="container">
+        <a href="https://www.pclinuxos.com/get-pclinuxos/">
+	       <img src="./assets/images/pclinux.png" alt="Mountains" ></a>
+    </div>
          <div class="container">
     <a href="http://puppylinux.com/download">
-	    <img src="./assets/images/puppy.png" alt="Mountains" style="height:100%; width: auto;">
+	    <img src="./assets/images/puppy.png" alt="Mountains" >
 </a>
     </div>
-       </div>
-  </div>
-<!-- END GRID -->
-</div>
-          <div class="row2">
-  <div class="column2">
-    <div class="content2">
         <div class="container">
            <a href="https://zorinos.com/download/">
-		   <img src="./assets/images/zorin.png" alt="Mountains" style="height:100%; width: auto;">
+		   <img src="./assets/images/zorin.png" alt="Mountains" >
 </a>
       </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
          <div class="container">
       <a href="https://www.bunsenlabs.org/installation.html">
-	      <img src="./assets/images/bunsen.png" alt="Lights" style="height:100%; width: auto;">
+	      <img src="./assets/images/bunsen.png" alt="Lights" >
 </a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
          <div class="container">
       <a href="https://kubuntu.org/getkubuntu/">
-	      <img src="./assets/images/kubuntu.png" alt="Nature" style="height:100%; width: auto;">
+	      <img src="./assets/images/kubuntu.png" alt="Nature" >
 </a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
          <div class="container">
       <a href="https://sourceforge.net/projects/manjarolinux/files/">
-	      <img src="./assets/images/manjaro.png" alt="Mountains" style="height:100%; width: auto;">
+	      <img src="./assets/images/manjaro.png" alt="Mountains" >
 </a>
     </div>
-      </div>
-  </div>
-   <div class="column2">
-    <div class="content2">
          <div class="container">
     <a href="https://sourceforge.net/projects/bodhilinux/files/">
-	    <img src="./assets/images/bodhi.png" alt="Mountains" style="height:100%; width: auto;">
+	    <img src="./assets/images/bodhi.png" alt="Mountains" >
 </a>
     </div>
-       </div>
-  </div>
-<!-- END GRID -->
-</div>
-           <div class="row2">
-  <div class="column2">
-    <div class="content2">
         <div class="container">
            <a href="https://www.netrunner.com/download/">
-		   <img src="./assets/images/netrunner.png" alt="Mountains" style="height:100%; width: auto;">
+		   <img src="./assets/images/netrunner.png" alt="Mountains" >
 </a>
       </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
          <div class="container">
       <a href="https://neptuneos.com/en/download.html">
-	      <img src="./assets/images/neptune.png" alt="Lights" style="height:100%; width: auto;">
+	      <img src="./assets/images/neptune.png" alt="Lights" >
 </a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
          <div class="container">
       <a href="https://www.kali.org/downloads/">
-	      <img src="./assets/images/kali.png" alt="Nature" style="height:100%; width: auto;">
+	      <img src="./assets/images/kali.png" alt="Nature" >
 </a>
     </div>
-      </div>
-  </div>
-  <div class="column2">
-    <div class="content2">
          <div class="container">
       <a href="https://sourceforge.net/projects/os4online/files/">
-	      <img src="./assets/images/blacklab.png" alt="Mountains" style="height:100%; width: auto;">
+	      <img src="./assets/images/blacklab.png" alt="Mountains" >
 </a>
     </div>
-      </div>
-  </div>
-   <div class="column2">
-    <div class="content2">
          <div class="container">
     <a href="https://getsol.us/download/">
-	    <img src="./assets/images/solus.png" alt="Mountains" style="height:100%; width: auto;">
+	    <img src="./assets/images/solus.png" alt="Mountains" >
 </a>
     </div>
        </div>
-  </div>
-<!-- END GRID -->
-</div>
-            </div>
+    </div>
 
-        <footer>
+    <footer>
         <p>@2003 Team 3 Final Project | Web and Mobile II</p>
     </footer>
   
-  </body>
+</body>
 </html>
