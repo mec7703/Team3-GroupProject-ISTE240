d24c59e7c1aef6eac4baaa4f9e97d4a3c0733428
MikeReniere <37932322+MikeReniere@users.noreply.github.com>
2018-12-03 14:19:39 -0500

    Add files via upload

--- begin.php
+++ begin.php
@@ -7,12 +7,13 @@
     <link rel="stylesheet" type="text/css" href="./assets/css/proj.css"/>
     <link rel="stylesheet" type="text/css" href="./assets/css/videoSlide.css"/>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="assets/script/videoSlide.js"></script>
-
+     <script src="assets/script/videoSlide.js"></script>
+    
 </head>
 <body>
 <header>
     <?php require "assets/inc/navbar.html"; ?>
+    
 </header>
 
 <h2>Beginner Linux Commands</h2>
@@ -33,24 +34,24 @@
         <div class="slideshow-container" id="navIndex">
             <div class="mySlides fade 1" name="navigation">
                 <div class="numbertext">1 / 3</div>
-                <video src="assets/vid/pwd.mp4" controls></video>
+                <video src="assets/vid/navigation/pwd.mp4" controls></video>
                 <div class="text">pwd Video</div>
             </div>
 
             <div class="mySlides fade 2">
                 <div class="numbertext">2 / 3</div>
-                <video src="assets/vid/cd.mp4" controls></video>
+                <video src="assets/vid/navigation/cd.mp4" controls></video>
                 <div class="text">cd video</div>
             </div>
 
             <div class="mySlides fade 3">
                 <div class="numbertext">3 / 3</div>
-                <video src="assets/vid/ls.mp4" controls></video>
+                <video src="assets/vid/navigation/ls.mp4" controls></video>
                 <div class="text">ls video</div>
             </div>
 
-            <a class="prev" onclick="showSlides(-1, 1, 3, navIndex)">&#10094;</a>
-            <a class="next" onclick="showSlides(1, 1, 3, navIndex)">&#10095;</a>
+            <a class="prev" onclick="showSlides(-1, 1, 3, 'navIndex')">&#10094;</a>
+            <a class="next" onclick="showSlides(1, 1, 3, 'navIndex')">&#10095;</a>
         </div>
     </div><!-- end column div -->
 </div><!-- end row div-->
@@ -60,7 +61,6 @@
         <ul>
             <li>rm [filename or directory] - Deletes a file or directory.</li>
             <li>mkdir [directory name] - creates a directory with the name specified</li>
-            <li>touch [filename] - creates a file of any type</li>
             <li>mv [filename] [destination] - Moves a desired file to the specified destination. Can also take a second
                 file as an argument in place of a destination to overwrite the file
             </li>
@@ -76,25 +76,31 @@
     <div class="column">
         <div class="slideshow-container" id="manIndex">
             <div class="mySlides fade 1">
-                <div class="numbertext">1 / 3</div>
-                <video src="assets/vid/rmdir.mp4" controls></video>
+                <div class="numbertext">1 / 4</div>
+                <video src="assets/vid/file_manip/rmdir.mp4" controls></video>
                 <div class="text">rmdir Video</div>
             </div>
 
             <div class="mySlides fade 2">
-                <div class="numbertext">2 / 3</div>
-                <video src="assets/vid/mkdir.mp4" controls></video>
+                <div class="numbertext">2 / 4</div>
+                <video src="assets/vid/file_manip/mkdir.mp4" controls></video>
                 <div class="text">mkdir video</div>
             </div>
 
             <div class="mySlides fade 3">
-                <div class="numbertext">3 / 3</div>
-                <video src="assets/vid/mvtoanotherfolder.mp4" controls></video>
-                <div class="text">move to another folder video</div>
+                <div class="numbertext">3 / 4</div>
+                <video src="assets/vid/file_manip/chmod.mp4" controls></video>
+                <div class="text">chmod video</div>
             </div>
 
-            <a class="prev" onclick="showSlides(-1, 4, 6, manIndex)">&#10094;</a>
-            <a class="next" onclick="showSlides(1, 4, 6, manIndex)">&#10095;</a>
+            <div class="mySlides fade 3">
+                <div class="numbertext">4 / 4</div>
+                <video src="assets/vid/file_manip/mvtoanotherfolder.mp4" controls></video>
+                <div class="text">move to another folder video</div>
+            </div>
+            
+            <a class="prev" onclick="showSlides(-1, 4, 7, 'manIndex')">&#10094;</a>
+            <a class="next" onclick="showSlides(1, 4, 7, 'manIndex')">&#10095;</a>
         </div>
     </div><!-- end column div -->
 </div><!-- end row div -->
@@ -102,8 +108,11 @@
     <div class="column">
         <h3>Application</h3>
         <ul>
+            
             <li>Cat [filename] - shows the content of a file</li>
-            <li>Nano [filename] - opens a tex editor for a file with the given name</li>
+            <li>vim [filename] - opens a text editor for a file with the given name</li>
+            <li>touch [filename] - creates a file of any type</li>
+            
         </ul>
     </div><!-- end column div -->
     <div class="column">
@@ -111,24 +120,24 @@
         <div class="slideshow-container" id="appIndex">
             <div class="mySlides fade 1">
                 <div class="numbertext">1 / 3</div>
-                <video src="assets/vid/pwd.mp4" controls></video>
-                <div class="text">pwd Video</div>
+                <video src="assets/vid/file_manip/cat.mp4" controls></video>
+                <div class="text">cat Video</div>
             </div>
 
             <div class="mySlides fade 2">
                 <div class="numbertext">2 / 3</div>
-                <video src="assets/vid/cd.mp4" controls></video>
-                <div class="text">cd video</div>
+                <video src="assets/vid/other/vim.mp4" controls></video>
+                <div class="text">vim video</div>
             </div>
 
             <div class="mySlides fade 3">
                 <div class="numbertext">3 / 3</div>
-                <video src="assets/vid/ls.mp4" controls></video>
-                <div class="text">ls video</div>
+                <video src="assets/vid/file_manip/touch.mp4" controls></video>
+                <div class="text">Touch video</div>
             </div>
 
-            <a class="prev" onclick="showSlides(-1, 7, 9, appIndex)">&#10094;</a>
-            <a class="next" onclick="showSlides(1, 7, 9, appIndex)">&#10095;</a>
+            <a class="prev" onclick="showSlides(-1, 8, 10, 'appIndex')">&#10094;</a>
+            <a class="next" onclick="showSlides(1, 8, 10, 'appIndex')">&#10095;</a>
         </div>
     </div><!-- end column div -->
 </div><!-- end row div -->
@@ -137,32 +146,32 @@
         <h3>Key "Shortcuts"</h3>
         <ul>
             <li>clear - enter into terminal to clear the terminal</li>
+            <li>exit - exits the terminal window</li>
             <li>Ctrl+C - stops any command/process that is currently running in the terminal</li>
-            <li>Ctrl+Z - force stops the current action/command</li>
         </ul>
     </div><!-- end column div -->
     <div class="column">
         <div class="slideshow-container" id="keyIndex">
             <div class="mySlides fade 1">
                 <div class="numbertext">1 / 3</div>
-                <video src="assets/vid/pwd.mp4" controls></video>
-                <div class="text">pwd Video</div>
+                <video src="assets/vid/shortcuts/clear.mp4" controls></video>
+                <div class="text">clear Video</div>
             </div>
 
             <div class="mySlides fade 2">
                 <div class="numbertext">2 / 3</div>
-                <video src="assets/vid/cd.mp4" controls></video>
-                <div class="text">cd video</div>
+                <video src="assets/vid/shortcuts/exit.mp4" controls></video>
+                <div class="text">exit video</div>
             </div>
 
             <div class="mySlides fade 3">
                 <div class="numbertext">3 / 3</div>
-                <video src="assets/vid/ls.mp4" controls></video>
-                <div class="text">ls video</div>
+                <video src="assets/vid/shortcuts/clear.mp4" controls></video>
+                <div class="text">clear video</div>
             </div>
 
-            <a class="prev" onclick="showSlides(-1, 10, 12, keyIndex)">&#10094;</a>
-            <a class="next" onclick="showSlides(1, 10, 12, keyIndex)">&#10095;</a>
+            <a class="prev" onclick="showSlides(-1, 11, 13, 'keyIndex')">&#10094;</a>
+            <a class="next" onclick="showSlides(1, 11, 13, 'keyIndex')">&#10095;</a>
         </div>
     </div><!-- end column div -->
 </div><!-- end row div -->
@@ -170,54 +179,47 @@
     <div class="column">
         <h3>Misc. Commands</h3>
         <ul>
-            <li>exit - exits the terminal window</li>
             <li>sudo reboot - restarts the computer</li>
             <li> man [command] - shows the manual page that gives more information and syntax on a specific command.
                 (ex: man cd)
             </li>
             <li>sudo [command] - runs a command with admin or root privelages (ex: sudo ls)</li>
             <li>df - shows the available disk space on each partition of the current hard drive</li>
-            <li>tar [file] - compresses a file into a tar file.
-                tar -cvf - creates a tar file
-                tar -xvf decompresses a tar file
-                tar -tvf lists the contents of a tar file
-            </li>
             <li>uname - shows the information on the linux distribution</li>
             <li>hostname - prints out the hostname of the machine</li>
-            <li>ping [ipaddress] - sends packets to a specified network or address to test internet connection</li>
         </ul>
     </div><!-- end column div -->
     <div class="column">
         <div class="slideshow-container" id="misIndex">
             <div class="mySlides fade 1">
                 <div class="numbertext">1 / 3</div>
-                <video src="assets/vid/pwd.mp4" controls></video>
-                <div class="text">pwd Video</div>
+                <video src="assets/vid/other/sudo.mp4" controls></video>
+                <div class="text">sudo Video</div>
             </div>
 
             <div class="mySlides fade 2">
                 <div class="numbertext">2 / 3</div>
-                <video src="assets/vid/cd.mp4" controls></video>
-                <div class="text">cd video</div>
+                <video src="assets/vid/other/man.mp4" controls></video>
+                <div class="text">man video</div>
             </div>
 
             <div class="mySlides fade 3">
                 <div class="numbertext">3 / 3</div>
-                <video src="assets/vid/ls.mp4" controls></video>
-                <div class="text">ls video</div>
+                <video src="assets/vid/other/hostname.mp4" controls></video>
+                <div class="text">hostname video</div>
             </div>
 
-            <a class="prev" onclick="showSlides(-1, 13, 15, misIndex)">&#10094;</a>
-            <a class="next" onclick="showSlides(1, 13, 15, misIndex)">&#10095;</a>
+            <a class="prev" onclick="showSlides(-1, 14, 16, 'misIndex')">&#10094;</a>
+            <a class="next" onclick="showSlides(1, 14, 16, 'misIndex')">&#10095;</a>
         </div>
     </div><!-- end column div -->
 </div><!-- end row div -->
 <script>
-    showSlides(0,1,3,navIndex);
-    showSlides(0,4,6,manIndex);
-    showSlides(0,7,9,appIndex);
-    showSlides(0,10,12,keyIndex);
-    showSlides(0,13,15,misIndex);
+    showSlides(0,1,3,'navIndex');
+    showSlides(0,4,7,'manIndex');
+    showSlides(0,8,10,'appIndex');
+    showSlides(0,11,13,'keyIndex');
+    showSlides(0,14,16,'misIndex');
 </script>
 </body>
 </html>
