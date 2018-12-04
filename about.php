5cc2fee356fc5eb5c8f1dd7cd9ce2e1143a265f5
jrr3147 <45493112+jrr3147@users.noreply.github.com>
2018-12-04 07:44:55 -0500

    include for head

--- about.php
+++ about.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<title>Linux</title>
-    
-    <link rel="stylesheet" type="text/css" href="./assets/css/proj.css" />
-</head>
+<?php require "assets/inc/head.html"; ?>
 <body>
 	<header>
        <?php 
