2d3c928bc6a83225644a59dd83fb26b7b4554bce
jrr3147 <45493112+jrr3147@users.noreply.github.com>
2018-12-04 06:01:59 -0500

    hamburger nav functionality

---
+++ assets/script/sidemenu.js
@@ -0,0 +1,12 @@
+/*
+Created by Jamie Rodriguez
+Date 12/4/18
+*/
+
+function openSideMenu(){
+	document.getElementById('side-menu').style.width = '100%'; //document.getElementById('main').style.marginLeft = '250px';
+} //end openSideMenu
+
+function closeSideMenu(){
+	document.getElementById('side-menu').style.width = '0'; //document.getElementById('main').style.marginLeft = '0';
+} //end openSideMenu
